f43b3edf589ce6c31adf2e319e01624a11b50a71
K M Masum Habib <masum.habib@gmail.com>
2014-11-27 00:39:58 +0000

    Add license and README.

---
+++ README.md
@@ -0,0 +1,7 @@
+Tiny Download Counter
+======================
+This is a tiny wordpress plugin that keeps track of download count 
+for multiple files. It uses an xml file to record the download count.
+
+There is no admin panel yet. Otherwise it is production ready.
+
--- tiny-download-counter.php
+++ tiny-download-counter.php
@@ -1,18 +1,24 @@
 <?php
 /*
 Plugin Name: Tiny Download Counter
-Plugin URI: http://github.org/masum.habib
+Plugin URI: https://github.com/masumhabib/tiny-download-counter
 Description: Counts how many times a given file has been being downloaded 
 Version: 0.02.0
 Author: K M Masum Habib
 Author URI: http://masumhabib.com/
 
-Released under the GPL version 2.0, http://www.gnu.org/licenses/gpl-2.0.html
+Released under the BSD license. See the license.txt for details.
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License version 2.0 for more details.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 
